51cdae7d0115d30303cbb5518e4f159aa0e4405e
crow-fox <surakamiya198@gmail.com>
2024-05-21 22:57:22 +0900

    main

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { toAscii } from "./ascii.ts";
+import { config } from "./config.ts";
+import { createDirIfNotExists, readEntries } from "./dir.ts";
+import { filterImageFile, readFileData, writeTextFile } from "./file.ts";
+import { getImageScale, readImageData } from "./image.ts";
+import { toMonochrome } from "./monochrome.ts";
+
+export async function main() {
+  const inputEntries = await readEntries(config.inputDir, config.maxImageCount);
+  const inputImageFilesEntries = inputEntries.filter(filterImageFile);
+  const inputImageFiles = await Promise.all(
+    inputImageFilesEntries.map((entry) => readFileData(config.inputDir, entry))
+  );
+  const inputImageDatas = await Promise.all(inputImageFiles.map(readImageData));
+
+  const asciiDatas = inputImageDatas.map((imageData) => {
+    const asciiString = toAscii(
+      toMonochrome(
+        imageData.data.scale(getImageScale(imageData.data, config.maxImageSize))
+      )
+    );
+    return {
+      name: imageData.name,
+      data: asciiString,
+    };
+  });
+
+  await createDirIfNotExists(config.outputDir);
+
+  await Promise.all(
+    asciiDatas.map(async ({ name, data }) => {
+      await writeTextFile(config.outputDir, name, data);
+    })
+  );
+
+  console.log(
+    "アスキーアートを作成しました",
+    asciiDatas.map((asciiArt) => asciiArt.name)
+  );
+}
